a38230c24521669ccfb484570d1699a242ddebdc
yugo-ops <yugokaneki@gmail.com>
2020-07-04 09:18:40 -0600

    Added node packages

---
+++ UChatBackend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const methodOverride =require("method-override");
+const rootApi = require("./api/rootapi")
+const userApi = require("./api/userapi")
+
+
+//setting up environment
+var app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded());
+app.use(methodOverride());
+//app.use(cors({origin: "http"}));
+http.createServer(app).listen(9000, () => {
+    console.log("Server start on port 9000")
+});
+
+app.use("/", rootApi);
+app.use("/user", userApi);
