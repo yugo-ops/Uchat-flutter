590b67e979ee1eea763e3fe239c889bb221b47f0
yugo-ops <yugokaneki@gmail.com>
2020-07-04 11:42:14 -0600

    landing page

--- UChatMobile/lib/main.dart
+++ UChatMobile/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:uchat_flutter_01/pages/LandingPage.dart';
 import 'package:uchat_flutter_01/pages/LoginPage.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: "/login",
+    initialRoute: "/landingpage",
     routes: {
+      "/landingpage" : (context) => LandingPage(),
       "/login" : (context) => LoginPage(),
     },
   ));
--- UChatMobile/lib/pages/LandingPage.dart
+++ UChatMobile/lib/pages/LandingPage.dart
@@ -6,13 +6,22 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 5), () => Navigator.pushNamed(context, '/login'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
         body: Center(
           child: SizedBox(
-
+            height: 100,
+            child: Image.asset('assets/landing_image.jpg', fit: BoxFit.contain,),
           ),
         )
     );
